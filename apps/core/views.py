833e98b36bbe897d4a750ecc9c4dc5e84317198c
DmevalN <dmeval.neto@salvador.ba.gov.br>
2022-08-08 11:58:37 -0300

    Start projeto

---
+++ apps/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    #a = 10
+    #bb = 'teste jkdasfhasdkfhasdfhjkasdf'
+    
+    return render(request, 'core/index.html')
